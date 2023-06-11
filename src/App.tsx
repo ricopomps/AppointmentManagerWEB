3d4fa0899ba2ec0c5236e7be0cd841dafbf674d2
Ricardo Pompilio <ricardopompiliodossantos@gmail.com>
2023-06-11 19:50:30 -0300

    Feat: Updating and deleting

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,22 @@
 import React, { useEffect, useState } from "react";
+import { FaPlus } from "react-icons/fa";
 import { Note as NoteModel } from "./models/note";
 import Note from "./components/Note";
-import { Col, Container, Row } from "react-bootstrap";
+import { Button, Col, Container, Row } from "react-bootstrap";
 import styles from "./styles/NotesPage.module.css";
+import stylesUtils from "./styles/utils.module.css";
+import * as NotesApi from "./network/notes_api";
+import AddNoteDialog from "./components/AddEditNoteDialog";
 
 function App() {
   const [notes, setNotes] = useState<NoteModel[]>([]);
+  const [showAddNoteDialog, setShowAddNoteDialog] = useState(false);
+  const [noteToEdit, setNoteToEdit] = useState<NoteModel | null>(null);
 
   useEffect(() => {
     async function loadNotes() {
       try {
-        const response = await fetch("/api/notes", {
-          method: "GET",
-        });
-        console.log(response);
-        const notes = await response.json();
+        const notes = await NotesApi.fetchNotes();
         setNotes(notes);
       } catch (error) {
         console.error(error);
@@ -24,15 +26,62 @@ function App() {
     loadNotes();
   }, []);
 
+  async function deleteNote(note: NoteModel) {
+    try {
+      await NotesApi.deleteNote(note._id);
+      setNotes(notes.filter((existingNote) => existingNote._id !== note._id));
+    } catch (error) {
+      console.log(error);
+      alert(error);
+    }
+  }
+
   return (
     <Container>
+      <Button
+        className={`mb-4 ${stylesUtils.blockCenter} ${stylesUtils.flexCenter}`}
+        onClick={() => setShowAddNoteDialog(true)}
+      >
+        <FaPlus />
+        Adicionar nova nota
+      </Button>
       <Row xs={1} md={2} xl={3} className="g-4">
         {notes.map((note) => (
           <Col key={note._id}>
-            <Note note={note} className={styles.note}></Note>
+            <Note
+              note={note}
+              onNoteClicked={setNoteToEdit}
+              onDeleteNoteClicked={deleteNote}
+              className={styles.note}
+            ></Note>
           </Col>
         ))}
       </Row>
+      {showAddNoteDialog && (
+        <AddNoteDialog
+          onDismiss={() => setShowAddNoteDialog(false)}
+          onNoteSaved={(newNote) => {
+            setNotes([...notes, newNote]);
+            setShowAddNoteDialog(false);
+          }}
+        />
+      )}
+      {noteToEdit && (
+        <AddNoteDialog
+          onDismiss={() => setNoteToEdit(null)}
+          noteToEdit={noteToEdit}
+          onNoteSaved={(updatedNote) => {
+            setNotes(
+              notes.map((existingNote) =>
+                existingNote._id === updatedNote._id
+                  ? updatedNote
+                  : existingNote
+              )
+            );
+            setNoteToEdit(null);
+          }}
+        />
+      )}
     </Container>
   );
 }
--- src/components/Note.tsx
+++ src/components/Note.tsx
@@ -1,14 +1,22 @@
 import { Card } from "react-bootstrap";
 import styles from "../styles/Note.module.css";
+import stylesUtils from "../styles/utils.module.css";
 import { Note as NoteModel } from "../models/note";
 import { formatDate } from "../utils/formatDate";
-
+import { MdDelete } from "react-icons/md";
 interface NoteProps {
   note: NoteModel;
+  onNoteClicked: (note: NoteModel) => void;
+  onDeleteNoteClicked: (note: NoteModel) => void;
   className?: string;
 }
 
-const Note = ({ note, className }: NoteProps) => {
+const Note = ({
+  note,
+  onNoteClicked,
+  onDeleteNoteClicked,
+  className,
+}: NoteProps) => {
   const { title, text, createdAt, updatedAt } = note;
 
   let createdUpdatedText: string;
@@ -19,9 +27,21 @@ const Note = ({ note, className }: NoteProps) => {
   }
 
   return (
-    <Card className={`${styles.noteCard} ${className}`}>
+    <Card
+      onClick={() => onNoteClicked(note)}
+      className={`${styles.noteCard} ${className}`}
+    >
       <Card.Body className={styles.cardBody}>
-        <Card.Title>{title}</Card.Title>
+        <Card.Title className={stylesUtils.flexCenter}>
+          {title}
+          <MdDelete
+            className="text-muted ms-auto"
+            onClick={(e) => {
+              onDeleteNoteClicked(note);
+              e.stopPropagation();
+            }}
+          />
+        </Card.Title>
         <Card.Text className={styles.noteText}>{text}</Card.Text>
       </Card.Body>
       <Card.Footer className="text-muted">{createdUpdatedText}</Card.Footer>
