1331127e10c4211b0dbac62f1a6641acdbd3f8e9
Ricardo Pompilio <ricardopompiliodossantos@gmail.com>
2023-06-11 23:30:31 -0300

    Feat: Routing with react router dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
-import { Container } from "react-bootstrap";
+import { useEffect, useState } from "react";
 import NavBar from "./components/NavBar/NavBar";
 import SignUpModal from "./components/SignUpModal";
 import LoginModal from "./components/loginModal";
-import styles from "./styles/NotesPage.module.css";
-import { useEffect, useState } from "react";
 import { User } from "./models/user";
 import * as NotesApi from "./network/notes_api";
-import NotesPageLoggedInView from "./components/NotesPageLoggedInView";
-import NotesPageLoggedOutView from "./components/NotesPageLoggedOutView";
-
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Container } from "react-bootstrap";
+import NotesPage from "./pages/NotesPages";
+import PrivacyPage from "./pages/PrivacyPage";
+import NotFoundPage from "./pages/NotFoundPage";
+import styles from "./styles/App.module.css";
 function App() {
   const [loggedInUser, setLoggedInUser] = useState<User | null>(null);
 
@@ -24,17 +25,27 @@ function App() {
         console.error(error);
       }
     }
+    fetchLoggedInUser();
   }, []);
   return (
-    <div>
-      <NavBar
-        loggedInUser={loggedInUser}
-        onLoginClicked={() => setShowLoginModal(true)}
-        onSignUpClicked={() => setShowSignUpModal(true)}
-        onLogoutSuccessful={() => setLoggedInUser(null)}
-      />
-      <Container className={styles.notesPage}>
-        {loggedInUser ? <NotesPageLoggedInView /> : <NotesPageLoggedOutView />}
+    <BrowserRouter>
+      <div>
+        <NavBar
+          loggedInUser={loggedInUser}
+          onLoginClicked={() => setShowLoginModal(true)}
+          onSignUpClicked={() => setShowSignUpModal(true)}
+          onLogoutSuccessful={() => setLoggedInUser(null)}
+        />
+        <Container className={styles.pageContainer}>
+          <Routes>
+            <Route
+              path="/"
+              element={<NotesPage loggedInUser={loggedInUser} />}
+            />
+            <Route path="/privacy" element={<PrivacyPage />} />
+            <Route path="/*" element={<NotFoundPage />} />
+          </Routes>
+        </Container>
         {showSignUpModal && (
           <SignUpModal
             onDismiss={() => setShowSignUpModal(false)}
@@ -53,8 +64,8 @@ function App() {
             }}
           />
         )}
-      </Container>
-    </div>
+      </div>
+    </BrowserRouter>
   );
 }
 
--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -2,6 +2,7 @@ import { Container, Nav, Navbar } from "react-bootstrap";
 import { User } from "../../models/user";
 import NavBarLoggedInView from "./NavBarLoggedInView";
 import NavBarLoggedOutView from "./NavBarLoggedOutView";
+import { Link } from "react-router-dom";
 
 interface NavBarProps {
   loggedInUser: User | null;
@@ -18,9 +19,16 @@ const NavBar = ({
   return (
     <Navbar bg="primary" variant="dark" expand="sm" sticky="top">
       <Container>
-        <Navbar.Brand>Notas</Navbar.Brand>
+        <Navbar.Brand as={Link} to="/">
+          Notas
+        </Navbar.Brand>
         <Navbar.Toggle aria-controls="main-navbar" />
         <Navbar.Collapse id="main-navbar">
+          <Nav>
+            <Nav.Link as={Link} to="/privacy">
+              Privacidade
+            </Nav.Link>
+          </Nav>
           <Nav className="ms-auto">
             {loggedInUser ? (
               <NavBarLoggedInView
