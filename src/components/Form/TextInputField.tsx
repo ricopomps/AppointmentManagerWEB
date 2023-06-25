6607448b9358da9eefe28d389795c98d766334c7
Ricardo Pompilio <ricardopompiliodossantos@gmail.com>
2023-06-25 12:41:56 -0300

    Refactor: Integrated select in textInputField

--- src/components/Form/TextInputField.tsx
+++ src/components/Form/TextInputField.tsx
@@ -1,12 +1,16 @@
 import { Form } from "react-bootstrap";
 import { RegisterOptions, UseFormRegister, FieldError } from "react-hook-form";
-
+interface option {
+  value: string;
+  key: string;
+}
 interface TextInputFieldProps {
   name: string;
   label: string;
   register: UseFormRegister<any>;
   registerOptions?: RegisterOptions;
   error?: FieldError;
+  options?: option[];
   [x: string]: any;
 }
 
@@ -16,6 +20,7 @@ const TextInputField = ({
   register,
   registerOptions,
   error,
+  options,
   ...props
 }: TextInputFieldProps) => {
   return (
@@ -25,7 +30,11 @@ const TextInputField = ({
         {...props}
         {...register(name, registerOptions)}
         isInvalid={!!error}
-      />
+      >
+        {options?.map((option) => (
+          <option value={option.value}>{option.key}</option>
+        ))}
+      </Form.Control>
       <Form.Control.Feedback type="invalid">
         {error?.message}
       </Form.Control.Feedback>
