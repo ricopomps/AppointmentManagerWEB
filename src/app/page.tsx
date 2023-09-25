850260c464d3e47e07c794089cb8e843a41187ba
Ricardo Pompilio <ricardopompiliodossantos@gmail.com>
2023-09-25 19:07:37 -0300

    Feat: Creating Clinic info

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,12 @@
 "use client";
 
+import logo from "@/assets/images/logo.png";
 import ProfileImage from "@/components/ProfileImage";
 import { generateIntervals } from "@/utils/prepareIntervals";
-import { Button, Col } from "react-bootstrap";
+import { capitalizeFirstLetter } from "@/utils/utils";
+import Image from "next/image";
+import { Button, Col, Nav, Row } from "react-bootstrap";
+import styles from "./Home.module.css";
 
 export interface Interval {
   interval: string;
@@ -11,15 +15,52 @@ export interface Interval {
   breakStartTime?: string;
   breakEndTime?: string;
 }
+
 export default function Home() {
+  const week = Array.from({ length: 7 }, (_, index) => {
+    const dayValue = new Date(); // Initialize with the current date
+    dayValue.setDate(dayValue.getDate() + index); // Increment the date for each day
+
+    // Define the Portuguese day names
+    const dayNames = [
+      "segunda-feira",
+      "terça-feira",
+      "quarta-feira",
+      "quinta-feira",
+      "sexta-feira",
+      "sábado",
+      "domingo",
+    ];
+
+    return {
+      dayNumber: index + 1, // Adding 1 to make it 1-based
+      dayName: dayNames[index],
+      dayValue: dayValue,
+    };
+  });
+
+  //put consultorio name and day on query params
+
   return (
-    <div>
-      <DentistGuy />
+    <div className="d-flex gap-4">
+      <ClinicView />
+      <div>
+        <Nav variant="tabs" defaultActiveKey={1}>
+          {week.map((day) => (
+            <Nav.Item key={day.dayValue.toString()}>
+              <Nav.Link eventKey={day.dayNumber}>
+                {capitalizeFirstLetter(day.dayName)}
+              </Nav.Link>
+            </Nav.Item>
+          ))}
+        </Nav>
+        <DentistSchedules />
+      </div>
     </div>
   );
 }
 
-function DentistGuy() {
+function DentistSchedules() {
   const intervalValues: Interval = {
     interval: "00:30:00",
     startTime: "08:00:00",
@@ -72,11 +113,62 @@ function DentistGuy() {
                       </Button>
                     );
                   })}
+                <hr />
               </Col>
             </div>
           </div>
         );
       })}
+    </div>
+  );
+}
+
+function ClinicView() {
+  const consultorios = [
+    { name: "Abreu e lima" },
+    { name: "Igarassu" },
+    { name: "Consultório novo" },
+  ];
+  return (
+    <div className={`p-4 ${styles.border}`}>
+      <Nav variant="tabs" defaultActiveKey={1}>
+        {consultorios.map((consultorio) => (
+          <Nav.Item key={consultorio.name}>
+            <Nav.Link eventKey={consultorio.name}>
+              {capitalizeFirstLetter(consultorio.name)}
+            </Nav.Link>
+          </Nav.Item>
+        ))}
+      </Nav>
+      <>
+        <Row>
+          <Col sm="auto">
+            <Image
+              src={logo}
+              width={200}
+              height={200}
+              alt={`Profile pic user: ${"username"}`}
+              priority
+              className={`rounded ${styles.profilePic}`}
+            />
+          </Col>
+          <Col className="mt-2 mt-sm-0">
+            <h1>{"displayName"}</h1>
+            <div>
+              <strong>Username: </strong>
+              {"username"}
+            </div>
+            <div>
+              <strong>User since: </strong>
+              {"formatDate(createdAt)"}
+            </div>
+            <div className="pre-line">
+              <strong>About me: </strong> <br />
+              {"about" || "This user hasn't shared any info yet"}
+            </div>
+          </Col>
+        </Row>
+      </>
     </div>
   );
 }
--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -57,3 +57,13 @@ export function handleError(error: unknown) {
 export function isServer() {
   return typeof window === "undefined";
 }
+
+export function capitalizeFirstLetter(str: string) {
+  const words = str.split("-");
+
+  const capitalizedWords = words.map((word) => {
+    return word.charAt(0).toUpperCase() + word.slice(1);
+  });
+
+  return capitalizedWords.join("-");
+}
