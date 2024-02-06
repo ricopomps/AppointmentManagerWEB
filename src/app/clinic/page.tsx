4094e2e2f9fefcbf2ad9f9c3ff0618f739bd424d
Ricardo Pompilio <ricardopompiliodossantos@gmail.com>
2024-02-05 22:53:52 -0300

    style: making a card for the clinic

--- src/app/clinic/page.tsx
+++ src/app/clinic/page.tsx
@@ -2,7 +2,6 @@
 import { UserContext } from "@/context/UserProvider";
 import { useUser } from "@clerk/nextjs";
 import { useContext } from "react";
-
 export default function ClinicPage() {
   const { clinic: selectedClinic } = useContext(UserContext);
 
@@ -12,12 +11,12 @@ export default function ClinicPage() {
 
   return (
     <main className="m-auto min-w-[300px] max-w-7xl p-4">
-      {JSON.stringify(selectedClinic)}
-      <div>
-        {user &&
-          user.publicMetadata.clinics
-            ?.find((clinic) => clinic.clinicId === selectedClinic.id)
-            ?.roles.map((role) => <p key={role}>{role}</p>)}
+      <div className="flex justify-center">
+        <div className="card w-96 bg-secondary shadow-xl">
+          <div className="card-body">
+            <h2 className="card-title">{selectedClinic.name}</h2>
+          </div>
+        </div>
       </div>
     </main>
   );
